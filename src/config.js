b564e1af923a7c34682d67f727aa0a06f61fc258
Seth Anderson <sdande02@gmail.com>
2020-06-20 17:23:03 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 // console.log('env', process.env)
 
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api'
+    // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api'
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://tranquil-badlands-46681.herokuapp.com/api'
 }
 
