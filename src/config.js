a4a7ccc254c90028b19ea474c585eb8da63de8ab
Seth Anderson <sdande02@gmail.com>
2020-06-23 14:40:30 -0700

    cleans up config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
-// console.log('env', process.env)
-
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api'
-    // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://tranquil-badlands-46681.herokuapp.com/api'
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT
 }
-
