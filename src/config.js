3b145da8d28165df77afe3812188a56c63b95aca
Seth Anderson <sdande02@gmail.com>
2020-06-23 14:33:14 -0700

    added REACT_APP_API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // console.log('env', process.env)
 
 export default {
-    // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api'
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://tranquil-badlands-46681.herokuapp.com/api'
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api'
+    // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://tranquil-badlands-46681.herokuapp.com/api'
 }
 
