e25cb308789c066348d20ba3eb5951c7672b0a9a
Seth Anderson <sdande02@gmail.com>
2020-06-23 17:08:29 -0700

    added normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import { BrowserRouter } from 'react-router-dom';
+import 'normalize.css';
 import './index.css';
 
 ReactDOM.render(
